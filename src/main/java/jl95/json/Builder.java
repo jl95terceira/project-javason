3fc5fd273d78587d07d4788017de8f550834222b
jl95terceira <jl95terceira@gmail.com>
2025-03-26 11:51:56 +0000

    small refactor

--- src/main/java/jl95/json/Builder.java
+++ src/main/java/jl95/json/Builder.java
@@ -4,12 +4,29 @@ import java.util.LinkedList;
 
 public abstract class Builder<N> {
 
-    private final String        repr;
-    private final LinkedList<N> stack    = new LinkedList<>();
-    private       N             root     = null;
-    private       String        entryKey = null;
+    public interface BuildMethods<N> {
+        N getNull  ();
+        N getTrue  ();
+        N getFalse ();
+        N getNumber(String numberRepr);
+        N getString(String s);
+        N getArray ();
+        N getObject();
+        Boolean isArrayOrObject(N x);
+        void addToArray (N array,            N v);
+        void addToObject(N object, String k, N v);
+    }
 
-    protected Builder(String repr) {this.repr = repr;}
+    private final BuildMethods<N> b;
+    private final String          repr;
+    private final LinkedList<N>   stack    = new LinkedList<>();
+    private       N               root     = null;
+    private       String          entryKey = null;
+
+    protected Builder(String repr) {
+        this.repr = repr;
+        this.b    = getBuildMethods();
+    }
 
     private void handleValue(N x) {
         if (root == null) {
@@ -18,13 +35,13 @@ public abstract class Builder<N> {
         else /* if we have a root already, the stack is guaranteed not to be empty */ {
             N parent = stack.getLast();
             if (entryKey == null) {
-                addToArray(parent, x);
+                b.addToArray(parent, x);
             } else {
-                addToObject(parent, entryKey, x);
+                b.addToObject(parent, entryKey, x);
                 entryKey = null;
             }
         }
-        if (isArrayOrObject(x)) {
+        if (b.isArrayOrObject(x)) {
             stack.add(x);
         }
     }
@@ -33,48 +50,39 @@ public abstract class Builder<N> {
 
         new StreamParser().parse(repr, new StreamParser.Handlers() {
 
-            @Override public void handleNull() {
-                handleValue(getNull());
+            @Override public void handleNull        () {
+                handleValue(b.getNull());
             }
-            @Override public void handleNumber(String nRepr) {
-                handleValue(getNumber(nRepr));
+            @Override public void handleNumber      (String nRepr) {
+                handleValue(b.getNumber(nRepr));
             }
-            @Override public void handleString(String s) {
-                handleValue(getString(s));
+            @Override public void handleString      (String s) {
+                handleValue(b.getString(s));
             }
-            @Override public void handleTrue() {
-                handleValue(getTrue());
+            @Override public void handleTrue        () {
+                handleValue(b.getTrue());
             }
-            @Override public void handleFalse() {
-                handleValue(getFalse());
+            @Override public void handleFalse       () {
+                handleValue(b.getFalse());
             }
-            @Override public void handleArrayStart() {
-                handleValue(getArray());
+            @Override public void handleArrayStart  () {
+                handleValue(b.getArray());
             }
-            @Override public void handleArrayEnd() {
+            @Override public void handleArrayEnd    () {
                 stack.removeLast();
             }
-            @Override public void handleObjectStart() {
-                handleValue(getObject());
+            @Override public void handleObjectStart () {
+                handleValue(b.getObject());
             }
-            @Override public void handleObjectEnd() {
+            @Override public void handleObjectEnd   () {
                 stack.removeLast();
             }
-            @Override public void handleObjectKey(String k) {
+            @Override public void handleObjectKey   (String k) {
                 entryKey = k;
             }
         });
         return root;
     }
 
-    protected abstract N getNull  ();
-    protected abstract N getTrue  ();
-    protected abstract N getFalse ();
-    protected abstract N getNumber(String numberRepr);
-    protected abstract N getString(String s);
-    protected abstract N getArray ();
-    protected abstract N getObject();
-    protected abstract Boolean isArrayOrObject(N x);
-    protected abstract void addToArray (N array,            N v);
-    protected abstract void addToObject(N object, String k, N v);
+    protected abstract BuildMethods<N> getBuildMethods();
 }
--- src/test/java/jl95/json/NodeBuilder.java
+++ src/test/java/jl95/json/NodeBuilder.java
@@ -9,48 +9,53 @@ public class NodeBuilder extends Builder<Node> {
     private NodeBuilder(String repr) {super(repr);}
 
     @Override
-    protected Node getNull() {
-        return Node.Null();
-    }
-    @Override
-    protected Node getTrue() {
-        return Node.Bool(true);
-    }
-    @Override
-    protected Node getFalse() {
-        return Node.Bool(false);
-    }
-    @Override
-    protected Node getNumber(String numberRepr) {
-        return Node.Int(Long.valueOf(numberRepr));
-    }
-    @Override
-    protected Node getString(String s) {
-        return Node.Str(s);
-    }
-    @Override
-    protected Node getArray() {
-        return Node.List();
-    }
-    @Override
-    protected Node getObject() {
-        return Node.Map();
-    }
-    @Override
-    protected Boolean isArrayOrObject(Node x) {
-        switch (x.type()) {
-        case LIST:
-        case MAP:
-            return true;
-        }
-        return false;
-    }
-    @Override
-    protected void addToArray(Node array, Node v) {
-        array.add(v);
-    }
-    @Override
-    protected void addToObject(Node object, String k, Node v) {
+    protected BuildMethods<Node> getBuildMethods() {
+        return new BuildMethods<Node>() {
+            @Override
+            public Node getNull() {
+                return Node.Null();
+            }
+            @Override
+            public Node getTrue() {
+                return Node.Bool(true);
+            }
+            @Override
+            public Node getFalse() {
+                return Node.Bool(false);
+            }
+            @Override
+            public Node getNumber(String numberRepr) {
+                return Node.Int(Long.valueOf(numberRepr));
+            }
+            @Override
+            public Node getString(String s) {
+                return Node.Str(s);
+            }
+            @Override
+            public Node getArray() {
+                return Node.List();
+            }
+            @Override
+            public Node getObject() {
+                return Node.Map();
+            }
+            @Override
+            public Boolean isArrayOrObject(Node x) {
+                switch (x.type()) {
+                case LIST:
+                case MAP:
+                    return true;
+                }
+                return false;
+            }
+            @Override
+            public void addToArray(Node array, Node v) {
+                array.add(v);
+            }
+            @Override
+            public void addToObject(Node object, String k, Node v) {
         object.setItem(k, v);
+    }
+        };
     }
 }
